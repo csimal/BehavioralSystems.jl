2f35f5b474d74eb57b9dc6cfac4f5f33befe236c
csimal <cedric.simal@unamur.be>
2022-12-07 23:00:36 +0100

    Fix mpum

--- src/mpum.jl
+++ src/mpum.jl
@@ -12,17 +12,9 @@ Compute the complexity of the most powerful unfalsified model that generates `w`
 function complexity_mpum(w::AbstractMatrix)
     q,T = size(w)
     l_max = floor(Int, (T+1)/(q+1))
-    l_mpum = 1
-    m_mpum, n_mpum = estimate_complexity(w, l_mpum)
-    for l in 2:l_max
-        m, n = estimate_complexity(w,l)
-        if n > 0 && m > 0 && m < m_mpum || n < n_mpum
-            l_mpum = l
-            m_mpum = m
-            n_mpum = n
-        end
-    end
-    return m_mpum, n_mpum, l_mpum
+    m, n = estimate_complexity(w, l_max)
+    l = lag_datadriven(w, m, n)
+    return m, n, l
 end
 
 """
@@ -31,8 +23,8 @@ end
 Estimate the number of inputs `m` and states `n` of a LTI system based on one of its trajectories and assuming lag `l`.
 """
 function estimate_complexity(w::AbstractMatrix, l)
-    r_1 = rank(hankel_matrix(w,l+1))
-    r_2 = rank(hankel_matrix(w,l+2))
+    r_1 = rank(hankel_matrix(w,l-1))
+    r_2 = rank(hankel_matrix(w,l))
     m = r_2 - r_1
     n = r_1 - (l+1)*m
     return m, n
